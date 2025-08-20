b8b7353f3eadaf66b9ada26d4eb30e5a25031d95
AzizJakbaraliyev <azizjakbaraliyev454@gamil.com>
2025-08-20 16:46:45 +0500

    Finish

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+import DigitalGirlsSanctuary from "../../components/navbar";
+
+function Home() {
+  return (
+    <div>
+      <DigitalGirlsSanctuary />
+    </div>
+  );
+}
+
+export default Home;
